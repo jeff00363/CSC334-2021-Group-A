6d15526b9a47c6faa287a3bc6b6a0cd8d17eea14
bpatricka <arbachouskb1@southernct.edu>
2021-04-26 05:40:34 -0400

    more css

--- js/switch.js
+++ js/switch.js
@@ -6,7 +6,8 @@ const courses = document.querySelector('.courses');
 const calendar = document.querySelector('.calendar');
 const assignments = document.querySelector('.assignments');
 const grades = document.querySelector('.grades');
-
+const course_p = document.querySelectorAll('div.course-panel');
+const course_pa = document.querySelectorAll('div.course-panel-active');
 
 // add event listeners to side buttons
 for (let i=0;i<sidebtns.length;i++){
@@ -30,4 +31,23 @@ for (let i=0;i<sidebtns.length;i++){
         };
         }
     )
+}
+
+for (let i=0;i<course_pa.length;i++){
+    console.log(course_pa[i]);
+    course_pa[i].addEventListener('click', cp_switch);
+}
+
+for (let i=0;i<course_p.length;i++){
+    console.log(course_p[i]);
+    course_p[i].addEventListener('click', cp_switch);
+}
+
+function cp_switch() {
+    let active_p = document.querySelector('.course-panel-active');
+    active_p.className = 'course-panel';
+    let e = window.event;
+    e.target.className = 'course-panel-active';
+    // console.log(course_pa);
+    // console.log(course_p);
 }
